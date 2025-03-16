5af2a509d32646c3f3e80321883aa3810e35e2f2
C Govind Rao <raogovind845@gmail.com>
2025-03-17 00:29:10 +0530

    Added sorting algorithms

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class SelectionSort {
+
+    public static void selectionSort(int arr[]){
+        int n= arr.length;
+
+        for(int i=0;i<n;i++){
+            int min_idx=i;
+
+            for (int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_idx]){
+                  min_idx=j;
+                }
+            }
+
+            int temp=arr[i];
+            arr[i]=arr[min_idx];
+            arr[min_idx]=temp;
+        }
+    }
+
+    static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+       int arr[]={2,1,5,4,3};
+       selectionSort(arr);
+       printArray(arr);
+    }
+}
