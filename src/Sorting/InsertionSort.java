f4e1b7807d391c76d74a4071f3df889d6b02efb4
C Govind Rao <raogovind845@gmail.com>
2025-03-17 01:26:49 +0530

    Added sorting algorithms

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class InsertionSort {
+    static void insertionSort(int arr[]){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0&& arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+
+            }
+            arr[j+1]=key;
+        }
+    }
+    static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,3,1,2};
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
